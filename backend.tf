1eafcd9eddca058c5f68b5cbed10de3c0695169a
Mohammad Bari <44865421+mohammadrbari@users.noreply.github.com>
2021-05-10 16:45:04 +0100

    aded backend.tf file to store Terraform Remote state into S3

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "co-aws-tfstate"
     key    = "aws-co-poc/terraform.tfstate"
     region = "eu-west-2"
+    # pls change your own personal profile for aws access and secret key
     profile = "co-aws-poc"
   }
 }
